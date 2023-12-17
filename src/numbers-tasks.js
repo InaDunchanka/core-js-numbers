9be8f032b1c505575c7d733cde536ca3afdc5a3a
InaDunchanka <rabotazdesIT@yandex.ru>
2023-12-17 07:23:49 +0300

    feat: 1

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -18,8 +18,11 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height) {
+  const a = width;
+  const b = height;
+  const s = a * b;
+  return s;
 }
 
 /**
